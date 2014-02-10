0c80dd97eb8e4e23fe15bf2565b1cf9866eadcb5
Ben Dewey <bdewey01@hotmail.com>
2014-02-10 15:58:22 -0500

    optimized syntax on scheduler/dispose

---
+++ NewWinJS/common/codeformatting/codeformatting.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    "use strict";
+    
+    WinJS.UI.Pages.define("/common/codeformatting/codeformatting.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+
+            element.querySelector('#convert').addEventListener('click', function () {
+
+                var input = element.querySelector("#input").value;
+                if (input[0] == '<') {
+                    input = input.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+                }
+                var buffer = document.createElement("pre");
+                buffer.innerHTML = input;
+                buffer.className = "prettyprint";
+                var container = element.querySelector("#container");
+                container.appendChild(buffer);
+                prettyPrint(buffer);
+                
+                var output = element.querySelector("#output");
+                output.value = buffer.innerHTML;
+            });
+
+            prettyPrint(element);
+        }
+    });
+})();
--- NewWinJS/js/google-code-prettify/prettyprint-scheduled.js
+++ NewWinJS/js/google-code-prettify/prettyprint-scheduled.js
@@ -1,11 +1,11 @@
-﻿(function() {
-    var prettyPrintOld = prettyPrint;
+﻿//(function() {
+//    var prettyPrintOld = prettyPrint;
 
-    prettyPrint = function(element) {
+//    prettyPrint = function(element) {
 
-        WinJS.Utilities.Scheduler.schedule(function() {
-            prettyPrintOld(element);
-        }, WinJS.Utilities.Scheduler.Priority.idle);
+//        WinJS.Utilities.Scheduler.schedule(function() {
+//            prettyPrintOld(element);
+//        }, WinJS.Utilities.Scheduler.Priority.idle);
 
-    };
-})();
+//    };
+//})();
